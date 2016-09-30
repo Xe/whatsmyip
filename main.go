99d21a4b9267d028af3099ff3181180950ef6de2
Cadey Dodrill <me@christine.website>
2016-09-30 14:33:19 -0700

    Only print IP address

--- main.go
+++ main.go
@@ -2,18 +2,30 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"net/http"
+
+	"github.com/sebest/xff"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, r.RemoteAddr)
-		fmt.Fprintln(w, r.Header.Get("X-Forwarded-For"))
+		host, _, err := net.SplitHostPort(r.RemoteAddr)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprintln(w, err)
+		}
+
+		fmt.Fprintln(w, host)
 	})
 
 	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 	})
 
-	http.ListenAndServe(":9090", nil)
+	xffmw, _ := xff.Default()
+
+	mux := xffmw.Handler(http.DefaultServeMux)
+
+	http.ListenAndServe(":9090", mux)
 }
